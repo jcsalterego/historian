486b30e4850a232c05ec17d43291326537f3b9cc
Jerry Chen <jerry@apache.org>
2017-04-18 22:26:18 -0500

    Add search_length test

---
+++ test/test_search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+htest_search_length() {
+    local length=1024
+    echo \
+        | awk '{while (z++ < '$length') {printf "A"}}' \
+        > $sandbox/.bash_history
+    sandbox_hist import
+    actual=$(sandbox_hist_with_output search A \
+        | grep A \
+        | awk '{print $NF}' \
+          );
+    actual_length=$(echo $actual | awk '{print length($0);}')
+    assert_equal ${actual_length} $length "actual length"
+}
