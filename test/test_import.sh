fbd3f11cb85b363f632ab842c35fd6fab7a65112
Elias Kunnas <elias@kunnas.com>
2017-06-07 17:30:00 +0300

    shellcheck-proof test scripts

--- test/test_import.sh
+++ test/test_import.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+typeset sandbox
 
 htest_import_simple() {
-    cat >> $sandbox/.bash_history <<EOF
+    cat >> "$sandbox"/.bash_history <<EOF
 foo
 bar
 baz
@@ -14,7 +15,7 @@ EOF
 }
 
 htest_import_will_dedupe() {
-    cat >> $sandbox/.bash_history <<EOF
+    cat >> "$sandbox"/.bash_history <<EOF
 foo
 bar
 baz
@@ -29,26 +30,26 @@ EOF
 }
 
 htest_import_handles_funny_characters() {
-    cat >> $sandbox/.bash_history <<EOF
+    cat >> "$sandbox"/.bash_history <<EOF
 echo \`ls\`
 ls ./\$(hi)
 foo \\\\bar\\\\baz
 foo ; bar ; baz
 EOF
 
-    sandbox_hist_with_output import
+    sandbox_hist import
 
     row_count=$(sandbox_sql 'SELECT COUNT(*) FROM history;')
     assert_equal 4 "${row_count}" "rows imported by simple bash_history"
 
     sandbox_sql 'SELECT command FROM HISTORY ORDER BY id ASC;' \
-        > $sandbox/exported_commands.txt
-    diff $sandbox/.bash_history $sandbox/exported_commands.txt
+        > "$sandbox"/exported_commands.txt
+    diff "$sandbox"/.bash_history "$sandbox"/exported_commands.txt
     assert_equal 0 $? "exported commands should match"
 }
 
 htest_import_run_twice_will_do_nothing_the_second_time() {
-    cat >> $sandbox/.bash_history <<EOF
+    cat >> "$sandbox"/.bash_history <<EOF
 foo
 bar
 baz
@@ -66,7 +67,7 @@ EOF
 }
 
 htest_import_imbalanced_quotes() {
-    cat >> $sandbox/.bash_history <<EOF
+    cat >> "$sandbox"/.bash_history <<EOF
 foo
 "bar
 baz"
@@ -76,11 +77,11 @@ EOF
 
     sandbox_hist import
 
-    local tmp=$(mktemp)
-    sandbox_sql 'SELECT command FROM history ORDER BY id ASC;' > $tmp
-    diff $tmp $sandbox/.bash_history
+    local -r tmp=$(mktemp)
+    sandbox_sql 'SELECT command FROM history ORDER BY id ASC;' > "$tmp"
+    diff "$tmp" "$sandbox"/.bash_history
     assert_equal 0 $?
-    rm -f $tmp
+    rm -f "$tmp"
 }
 
 htest_import_zsh_extended_history_parses_correctly() {
--- test/test_search.sh
+++ test/test_search.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
+typeset sandbox
 
 htest_search_length() {
-    local length=1024
-    echo \
-        | awk '{while (z++ < '$length') {printf "A"}}' \
-        > $sandbox/.bash_history
+    local -i length=1024
+    printf '%0.sA' $(seq $length) > "$sandbox"/.bash_history
+    echo >> "$sandbox"/.bash_history
     sandbox_hist import
-    actual=$(sandbox_hist_with_output search A \
-        | grep A \
-        | awk '{print $NF}' \
-          );
-    actual_length=$(echo $actual | awk '{print length($0);}')
-    assert_equal ${actual_length} $length "actual length"
+    actual=$(
+        sandbox_hist_with_output search A |
+        grep A |
+        awk '{print $NF}'
+    )
+    actual_length=${#actual}
+    assert_equal "${actual_length}" $length "actual length"
 }
